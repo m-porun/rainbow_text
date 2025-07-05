c981c3c082843a6f54e3aee356cd11aa0d34eca3
m-porun <butachikusho@gmail.com>
2025-07-06 00:46:09 +0900

    [FIX]TranslationsControllerTestが古いメソッドのままになっていたので、現状のメソッドでテストするよう修正

--- test/controllers/translations_controller_test.rb
+++ test/controllers/translations_controller_test.rb
@@ -1,8 +1,30 @@
 require "test_helper"
 
 class TranslationsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get translations_index_url
+  # --- newアクションのテスト ---
+  test "should get new" do
+    get new_translation_url
+    assert_response :success
+  end
+
+  # --- createアクションのテスト ---
+  test "should create translation" do
+    assert_difference('Translation.count') do
+      # ここに translations#create アクションに送信するデータ（params）を記述します。
+      # 例: translationモデルに content:string がある場合
+      post translations_url, params: { translation: { content: "テストコンテンツ" } }
+    end
+    assert_redirected_to translation_url(Translation.last) # 作成後にshowページへリダイレクトされることを期待
+  end
+
+  # --- showアクションのテスト ---
+  test "should show translation" do
+    # テストデータを作成します。test_fixtures/translations.yml を使用することもできます。
+    # 例: @translation = translations(:one) のように fixture をロードするか、
+    # Translation.create!(content: "表示テスト") のように直接作成します。
+    @translation = Translation.create!(content: "テスト表示コンテンツ") # 仮にモデルにcontentカラムがあると仮定
+
+    get translation_url(@translation)
     assert_response :success
   end
 end
